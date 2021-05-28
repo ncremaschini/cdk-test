b175fc1442afcf3b90c0f14365e2eaa9f757027f
Nicola <nicola.cremaschini@skytv.it>
2021-05-28 11:49:19 +0200

    lambda tests

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -23,3 +23,25 @@ test('DynamoDB Table Created', () => {
         }
     }));
 });
+
+test('Lambda Has Environment Variables', () => {
+    const stack = new cdk.Stack();
+    // WHEN
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream: new lambda.Function(stack, 'TestFunction', {
+            runtime: lambda.Runtime.NODEJS_12_X,
+            handler: 'lambda.handler',
+            code: lambda.Code.fromInline('test')
+        })
+    });
+
+    // THEN
+    expect(stack).to(haveResource('AWS::Lambda::Function', {
+        Environment: {
+            Variables: {
+                DOWNSTREAM_FUNCTION_NAME: {"Ref": "TestFunction22AD90FC"},
+                HITS_TABLE_NAME: {"Ref": "MyTestConstructHits24A357F0"}
+            }
+        }
+    }));
+});
