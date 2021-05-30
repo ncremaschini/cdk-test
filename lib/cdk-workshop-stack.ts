d2460c072ac1e6fad457ef5064320b1d632f6f2e
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2021-05-30 22:16:35 +0200

    fix pipeline code

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,34 +1,35 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as apigw from '@aws-cdk/aws-apigateway';
-import { HitCounter } from './hitCounter';
-import { TableViewer } from 'cdk-dynamo-table-viewer';
-import { PipelineStack } from '../lib/pipeline-stack';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
+import { HitCounter } from "./hitCounter";
+import { TableViewer } from "cdk-dynamo-table-viewer";
+import { PipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
-new PipelineStack(app,'CdkPipelineStack');
+new PipelineStack(app, "CdkPipelineStack", {
+  githubToken: process.env.GITHUB_TOKEN!,
+});
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props)
-    const helloFunction = new lambda.Function(this, 'HelloFunction',
-      {
-        runtime: lambda.Runtime.NODEJS_12_X,
-        code: lambda.Code.fromAsset('lambda'),
-        handler: 'hello.handler'
-      });
-
-    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
-      downstream: helloFunction
+    super(scope, id, props);
+    const helloFunction = new lambda.Function(this, "HelloFunction", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
     });
 
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: helloWithCounter.handler
+    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: helloFunction,
     });
 
-    new TableViewer(this, 'ViewHitCounter',{
-      title: 'Hello Hits',
-      table: helloWithCounter.table
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: helloWithCounter.handler,
+    });
+
+    new TableViewer(this, "ViewHitCounter", {
+      title: "Hello Hits",
+      table: helloWithCounter.table,
     });
   }
 }
