aa08c8b5ece77778b7bce83a8542776cebf986e1
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2021-05-30 21:50:56 +0200

    cdk version uplift

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+import * as cdk from "@aws-cdk/core";
+import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
+
+if (!process.env.GITHUB_TOKEN) {
+  console.log("No Github Token present");
+}
 
 const app = new cdk.App();
-new CdkWorkshopStack(app, 'CdkWorkshopStack');
+new CdkWorkshopStack(app, "CdkWorkshopStack");
