7ef8305dfaef5eadb0e0a248d6d2ff20bbb65290
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2021-05-30 23:39:59 +0200

    fix tests

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -3,10 +3,6 @@ import * as cdk from "@aws-cdk/core";
 import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 import { PipelineStack } from "../lib/pipeline-stack";
 
-if (!process.env.CONNECTION_ARN) {
-  console.log("No connection arn present");
-}
-
 const app = new cdk.App();
 
 new PipelineStack(app, "CdkPipelineStack", {
--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from "@aws-cdk/core";
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
 import { SimpleSynthAction, CdkPipeline } from "@aws-cdk/pipelines";
 import { SecretValue, StackProps } from "@aws-cdk/core";
+import * as ssm from "@aws-cdk/aws-ssm";
 
 export interface PipelineStackProps extends StackProps {
-  readonly connection_arn: string;
+  connection_arn: string;
 }
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props);
+
+    if (!props.connection_arn) {
+      console.log("No connection arn present. fetching from ssm");
+      const connectionArn = ssm.StringParameter.valueForStringParameter(
+        this,
+        "connection_arn"
+      ); 
+      
+      props.connection_arn = connectionArn;
+    }
 
     // Defines the artifact representing the sourcecode
     const sourceArtifact = new codepipeline.Artifact();
@@ -23,25 +34,21 @@ export class PipelineStack extends cdk.Stack {
     new CdkPipeline(this, "Pipeline", {
       pipelineName: "WorkshopPipeline",
       cloudAssemblyArtifact,
-      
+
       // Generates the source artifact from the repo we created in the last step
       sourceAction: new codepipeline_actions.CodeStarConnectionsSourceAction({
-        actionName: 'Checkout',
+        actionName: "Checkout",
         output: sourceArtifact,
         owner: "ncremaschini",
         repo: "cdk-test",
         branch: "main",
-        connectionArn: props.connection_arn
+        connectionArn: props.connection_arn,
       }),
 
       // Builds our source code outlined above into a could assembly artifact
       synthAction: SimpleSynthAction.standardNpmSynth({
         sourceArtifact, // Where to get source code to build
         cloudAssemblyArtifact, // Where to place built source
-        environmentVariables:{
-          pippo: {value: 'pluto'},
-          CONNECTION_ARN: {value: props.connection_arn}
-        },
         buildCommand: "npm run build", // Language-specific build cmd
       }),
     });
